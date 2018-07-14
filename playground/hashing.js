3f4b5c7c9d74f9fe56e59b066f1e50f68419c080
Morshed Alam <morshed.alam@gagagugu.com>
2018-07-14 19:03:35 +0600

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password  = 'abac123!'
+
+// bcrypt.genSalt(10, (err, salt)=>{
+//     bcrypt.hash(password,salt,(err, hash)=>{
+//         console.log(hash);
+//     });
+// });
+
+var hashPassword = '$2a$10$eYkyf3n6lVUHXWiIrOAhs.TYDiesPiHGhbwgHU4CzglkSYjyAyJ/G'
+
+bcrypt.compare(password, hashPassword, (err, res)=>{
+    console.log(res);
+});
 // var msg = "hey i am newwork";
 // var hash = SHA256(msg).toString();
 
@@ -16,11 +30,11 @@ const jwt = require('jsonwebtoken');
 
 // console.log(token.hash);
 
-var data = {
-    id:4
-}
-var token  = jwt.sign(data, "some secret");
-console.log(token);
+// var data = {
+//     id:4
+// }
+// var token  = jwt.sign(data, "some secret");
+// console.log(token);
 
-var decoded = jwt.verify(token, 'some secret');
-console.log(decoded);
+// var decoded = jwt.verify(token, 'some secret');
+// console.log(decoded);
